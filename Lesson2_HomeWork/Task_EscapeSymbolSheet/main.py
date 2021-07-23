bc5af6507b4871269b10a30be02a84f7c612f36b
YokaEnenra <lartaman1337@gmail.com>
2021-07-23 16:51:19 +0300

    Упростил задание name Выполнил задание escape character sheet Выполнил задание speech emu

--- Lesson2_HomeWork/Task_EscapeSymbolSheet/main.py
+++ Lesson2_HomeWork/Task_EscapeSymbolSheet/main.py
@@ -1,6 +1,8 @@
-print("")
+print(
+    "This is ESCAPE Characters Sheet:\n" + r"\a - Bell(alert)" + "\n" + r"\b - Backspace" + "\n" + r"\n - New Line" + "\n" + r"\t - Horizontal Tab" + "\n" + r"\\ - Backslash(/)" + "\n" +
+    r'\" - Double Quotation Mark(")' + "\n" + r"\' - Single Quotation Mark(')")
 # Escape sequences
-# \a    -  Bell (alert)
+# \Name    -  Bell (alert)
 # \b    -  Backspace
 # \n    -  New line
 # \t    - Horizontal tab
---
+++ Lesson2_HomeWork/Task_SpeechEmu/README.md
@@ -0,0 +1,2 @@
+There is last, third, part of my second homework
+Task is very simple again, the only thing i must to do, is to create small program that will emulate Speech
