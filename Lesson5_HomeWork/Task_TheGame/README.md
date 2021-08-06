2db0bec6a57d9c93994af37202c03436e45ed0a8
YokaEnenra <lartaman1337@gmail.com>
2021-08-06 13:24:20 +0300

    Домашние задание №5, часть 2 Выполнено + описание

---
+++ Lesson5_HomeWork/Task_TheGame/README.md
@@ -0,0 +1,2 @@
+There is second part of my fifth homework.
+My task was to create program that will choose number from 1 to 100 and then help user to understand what this number is.
---
+++ Lesson5_HomeWork/Task_TheGame/game.py
@@ -0,0 +1,38 @@
+import random
+
+print(
+    'Приветствую вас в игре "Угадайка", я загадаю число от 1 до 100, а вы должны угадать это число, я буду давать '
+    'подсказки')
+while True:
+    is_win = False
+    right_answer = random.randint(1, 100)
+    loose_counter = 1
+    while not is_win:
+        print("И так, твой ответ?")
+        try:
+            usr_answer = int(input())
+            if right_answer == usr_answer and loose_counter == 1:
+                print("Ты угадал с первой попытки, ничего себе, я удивлён, правильным числом было", right_answer)
+            elif right_answer == usr_answer:
+                print("Ты угадал, победа за тобой, это заняло у тебя", loose_counter,
+                      "попыток, а правильным числом было", right_answer)
+                break
+            else:
+                if usr_answer > right_answer:
+                    print("Твое число больше, чем то, что я загадал")
+                else:
+                    print("Твое число меньше, чем то, что я загадал")
+            loose_counter += 1
+        except:
+            print("Ты ввел не число или дробное число")
+            loose_counter += 1
+    print('Хочешь сыграть еще? "y-да" "n-нет"')
+    usr_answer = input()
+    if usr_answer == 'n':
+        print("Ок, завершаю работу")
+        break
+    elif usr_answer != 'n' and usr_answer != 'y':
+        print('Не хотел играть, написал бы "n", зачем билиберду писать')
+        break
+    else:
+        print("Ок, продолжаем")
