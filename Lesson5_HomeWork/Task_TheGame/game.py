50ce04360b3511e921e703618302fc269132fb02
YokaEnenra <lartaman1337@gmail.com>
2021-08-06 14:35:44 +0300

    Исправление грамматических ошибок

--- Lesson5_HomeWork/Task_TheGame/game.py
+++ Lesson5_HomeWork/Task_TheGame/game.py
@@ -7,15 +7,15 @@ is_game=True
 while is_game:
     is_win = False
     right_answer = random.randint(1, 100)
-    loose_counter = 1
+    loss_counter = 1
     while not is_win:
         print("И так, твой ответ?")
         try:
             usr_answer = int(input())
-            if right_answer == usr_answer and loose_counter == 1:
+            if right_answer == usr_answer and loss_counter == 1:
                 print("Ты угадал с первой попытки, ничего себе, я удивлён, правильным числом было", right_answer)
             elif right_answer == usr_answer:
-                print("Ты угадал, победа за тобой, это заняло у тебя", loose_counter,
+                print("Ты угадал, победа за тобой, это заняло у тебя", loss_counter,
                       "попыток, а правильным числом было", right_answer)
                 is_win=True
             else:
@@ -23,10 +23,10 @@ while is_game:
                     print("Твое число больше, чем то, что я загадал")
                 else:
                     print("Твое число меньше, чем то, что я загадал")
-            loose_counter += 1
+            loss_counter += 1
         except:
             print("Ты ввел не число или дробное число")
-            loose_counter += 1
+            loss_counter += 1
     print('Хочешь сыграть еще? "y-да" "n-нет"')
     usr_answer = input()
     if usr_answer == 'n':
