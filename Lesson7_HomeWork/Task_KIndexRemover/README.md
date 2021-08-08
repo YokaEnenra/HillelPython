6193da71d43792dc793593be42844b7d10cfac64
YokaEnenra <lartaman1337@gmail.com>
2021-08-08 14:45:42 +0300

    Домашнее Задание 6, часть 1 Выполнено

---
+++ Lesson7_HomeWork/Task_KIndexRemover/README.md
@@ -0,0 +1,2 @@
+There is first part of my sixth homework.
+My task was to create program that will ask user a list of numbers and then delete element by his index with reindexing list after.
---
+++ Lesson7_HomeWork/Task_KIndexRemover/remover.py
@@ -0,0 +1,36 @@
+def remover(index=int(), array=list()):
+    for k in range(index, len(array) - 1):
+        array[k] = array[k + 1]
+    array.pop()
+    return True
+
+
+try:
+    print("Укажите кол-во елементов желаемого списка")
+    elem_number = int(input())
+    arr = list()
+    i = 0
+    while i < elem_number:
+        number = input("Следующий элемент списка: ")
+        try:
+            arr.append(int(number))
+            i += 1
+        except:
+            try:
+                arr.append(float(number))
+                i += 1
+            except:
+                print("Вы ввели не число")
+    is_rmv = False
+    while not is_rmv:
+        print("Укажите индекс элемента который желаете удалить")
+        rmv_index = int(input()) - 1
+        if rmv_index <= len(arr):
+            is_rmv = remover(rmv_index, arr)
+            for j in enumerate(arr):
+                print(j)
+        else:
+            print("Указанный индекс слишком велик, в массиве нет такого елемента")
+
+except:
+    print("Вы ввели не число или дробное число")
