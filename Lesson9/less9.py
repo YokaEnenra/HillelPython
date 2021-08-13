b127f48a0ffe40ab77e83b55efc14927bb44a56c
kitayka <kitayka96@gmail.com>
2021-08-13 18:53:51 +0300

    Урок 9

--- Lesson9/less9.py
+++ Lesson9/less9.py
@@ -1,7 +1,50 @@
-def hi():
-    """ Печатает hi"""  # Описание ф-кции
-    print('hi')
+# def hi():
+#     """ Печатает hi"""  # Описание ф-кции
+#     print('hi')
+#
+#
+# hello = hi
+# hello()
+
+# def func(*args):  # Ф-кция принимает список
+#     print(args)
+#     return args
+#
+#
+# func(1,2,3,'abc')
+
+# def func(**kwargs):  # Ф-кция принимает словарь
+#     print(kwargs)
+#     return kwargs
+#
+#
+# func(a=1,b=2,c=3,d='abc')
+
+# def func(c, a=int(), b=float()) -> int:
+#     print(a+b+c)
+#     return a
+#
+#
+# func(1, 3, 2.4)
+
+# f1 = lambda: 10+20
+# f2 = lambda x, y: x + y
+# f3 = lambda x, y, z: print(f1())
+# print(f1())
+# print(f2(5, 7))
+# print('*'*20)
+# print(f3(1, 2, 3))
+# print('*'*20)
+
+def gen_f(x, y):
+    for i in range(1, x+1):
+        yield i**y
 
 
-hello = hi
-hello()
+# for n in gen_f(15, 3):
+#     print(n, end=' ')
+f = gen_f(4, 2)
+print(f.__next__())
+print(f.__next__())
+print(f.__next__())
+print(f.__next__())
---
+++ Lesson9/most_word_try.py
@@ -0,0 +1,28 @@
+def lamb(my_dict=dict(),i=str):
+    return my_dict.get(i)
+
+
+with open('data.txt', 'r+') as f:
+    my_text = f.read()
+my_dict = dict()
+my_list = list(my_text.split())
+banned_list = (',', '.', '!', '?', ':', ';')
+for i in range(len(my_list)):
+    my_list[i] = my_list[i].lower()
+    for j in banned_list:
+        if j in my_list[i]:
+            my_list[i] = my_list[i].replace(j, '')
+for i in my_list:
+    if i not in my_dict:
+        counter = my_list.count(i)
+        my_dict.update({i: counter})
+biggest_amount = {'0': 0}
+biggest_amount_key = '0'
+# for i in my_dict:
+#     if my_dict.get(i) > biggest_amount.get(biggest_amount_key):
+#         biggest_amount = biggest_amount.clear()
+#         biggest_amount = {i: my_dict.get(i)}
+#         biggest_amount_key = i
+print(sorted(my_dict,lamb(my_dict,i)))
+print('Слово встречающееся в полученном тексте больше всего раз найдено, я выведу его в формате "слово: кол-во раз"\n'
+      , biggest_amount)
