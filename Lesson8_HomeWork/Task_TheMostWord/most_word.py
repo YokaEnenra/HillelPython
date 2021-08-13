45aaf3132de31e9e2d1f8baa98f5c296062b959c
YokaEnenra <lartaman1337@gmail.com>
2021-08-13 14:55:54 +0300

    Домашнее задание 7, часть 4 Переделано под большее использование словаря

--- Lesson8_HomeWork/Task_TheMostWord/most_word.py
+++ Lesson8_HomeWork/Task_TheMostWord/most_word.py
@@ -12,11 +12,12 @@ for i in my_list:
     if i not in my_dict:
         counter = my_list.count(i)
         my_dict.update({i: counter})
-biggest_amount = 0
-biggest_amount_word = str()
-for i in my_list:
-    if my_dict.get(i) > biggest_amount:
-        biggest_amount = my_dict.get(i)
-        biggest_amount_word = i
+biggest_amount = {'0': 0}
+biggest_amount_key = '0'
+for i in my_dict:
+    if my_dict.get(i) > biggest_amount.get(biggest_amount_key):
+        biggest_amount = biggest_amount.clear()
+        biggest_amount = {i: my_dict.get(i)}
+        biggest_amount_key = i
 print('Слово встречающееся в полученном тексте больше всего раз найдено, я выведу его в формате "слово: кол-во раз"\n'
-      + biggest_amount_word + ":", biggest_amount)
+      , biggest_amount)
