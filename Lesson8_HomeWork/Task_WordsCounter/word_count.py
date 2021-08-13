d0d1859f6c786f48865a5da11e59def3e3c498f9
YokaEnenra <lartaman1337@gmail.com>
2021-08-13 14:20:29 +0300

    Домашнее задание 7, часть 3 Изменено создание словаря на более короткое

--- Lesson8_HomeWork/Task_WordsCounter/word_count.py
+++ Lesson8_HomeWork/Task_WordsCounter/word_count.py
@@ -9,8 +9,8 @@ for i in range(len(my_list)):
         if j in my_list[i]:
             my_list[i] = my_list[i].replace(j, '')
 
-for i in range(len(my_list)):
-    if my_list[i] not in my_dict:
-        counter = my_list.count(my_list[i])
-        my_dict.update({my_list[i]: counter})
+for i in my_list:
+    if i not in my_dict:
+        counter = my_list.count(i)
+        my_dict.update({i: counter})
 print('Все слова в переданном тексте посчитаны сейчас я выведу результат в формате "слово: кол-во раз"\n', my_dict)
