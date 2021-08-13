4d86dbb337d27cc743fa6801abb6268f6f29fd9d
YokaEnenra <lartaman1337@gmail.com>
2021-08-13 14:22:45 +0300

    Домашнее задание 7, часть 4 Исправлена ошибка с невозможносртью существования двух слов с одинаковым кол-вом повторений Изменено создание словаря на более короткое Изменена проверка на знаки препинания и их удаление на более короткие и менее ресурсо затратные Изменен поиск слова с наибольшим кол-вом повторений в связи с переделкой создания словаря

--- Lesson8_HomeWork/Task_TheMostWord/most_word.py
+++ Lesson8_HomeWork/Task_TheMostWord/most_word.py
@@ -2,18 +2,21 @@ with open('data.txt', 'r+') as f:
     my_text = f.read()
 my_dict = dict()
 my_list = list(my_text.split())
+banned_list = (',', '.', '!', '?', ':', ';')
 for i in range(len(my_list)):
     my_list[i] = my_list[i].lower()
-    if ',' in my_list[i]:
-        my_list[i] = my_list[i].replace(',', '')
-    elif '.' in my_list[i]:
-        my_list[i] = my_list[i].replace('.', '')
-for i in range(len(my_list)):
-    if my_list[i] not in my_dict:
-        counter = my_list.count(my_list[i])
-        my_dict.update({counter: my_list[i]})
-keys_list = list(my_dict.keys())
-keys_list = sorted(keys_list)
-biggest_key = int(keys_list[-1])
-print('Слово встречающееся в полученном тексте больше всего раз найдено, я выведу его в формате "кол-во раз: слово"\n'
-      , str(biggest_key) + ': ' + my_dict.get(biggest_key))
+    for j in banned_list:
+        if j in my_list[i]:
+            my_list[i] = my_list[i].replace(j, '')
+for i in my_list:
+    if i not in my_dict:
+        counter = my_list.count(i)
+        my_dict.update({i: counter})
+biggest_amount = 0
+biggest_amount_word = str()
+for i in my_list:
+    if my_dict.get(i) > biggest_amount:
+        biggest_amount = my_dict.get(i)
+        biggest_amount_word = i
+print('Слово встречающееся в полученном тексте больше всего раз найдено, я выведу его в формате "слово: кол-во раз"\n'
+      + biggest_amount_word + ":", biggest_amount)
