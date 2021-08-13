76ff608db5c3e108ea8b5899a422a08afc48908c
YokaEnenra <lartaman1337@gmail.com>
2021-08-13 12:41:14 +0300

    Домашнее задание 7, часть 3 Выполнено

---
+++ Lesson8_HomeWork/Task_WordsCounter/README.md
@@ -0,0 +1,2 @@
+There is third part of my seventh homework.
+My task was to create program that will get text from user and then give user a result that says how many times each word is in text.
---
+++ Lesson8_HomeWork/Task_WordsCounter/word_count.py
@@ -0,0 +1,15 @@
+with open('data.txt', 'r+') as f:
+    my_text = f.read()
+my_dict = dict()
+my_list = list(my_text.split())
+for i in range(len(my_list)):
+    my_list[i] = my_list[i].lower()
+    if ',' in my_list[i]:
+        my_list[i] = my_list[i].replace(',', '')
+    elif '.' in my_list[i]:
+        my_list[i] = my_list[i].replace('.', '')
+for i in range(len(my_list)):
+    if my_list[i] not in my_dict:
+        counter = my_list.count(my_list[i])
+        my_dict.update({counter: my_list[i]})
+print('Все слова в переданном тексте посчитаны сейчас я выведу результат в формате "кол-во раз:слово"\n', my_dict)
