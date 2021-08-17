dc123842084b45e68941dc98df2696c35f2cb924
kitayka <kitayka96@gmail.com>
2021-08-17 17:41:19 +0300

    Домашнее Задание 8, часть 1 Исправлено

--- Lesson9_HomeWork/Task_IfSumInNumber/if_summ.py
+++ Lesson9_HomeWork/Task_IfSumInNumber/if_summ.py
@@ -7,14 +7,19 @@ def if_sum_in_list(my_list: list = [0], number: int = 1) -> bool:
         return False
 
 
-print("Дайте мне список и число и я скажу есть ли в этом списке два числа, сумма которых дает введенное вами число")
-try:
-    number_of_elements = int(input("Укажите число элементов списка: "))
-    a_list = [int(input("Следующий элемент списка: ")) for i in range(number_of_elements)]
-    number_for_search = int(input("Укажите число которое будем проверять: "))
-    if if_sum_in_list(a_list, number_for_search):
-        print("В вашем списке есть два числа в сумме дающих введенное вами число")
-    else:
-        print("В вашем списке нету двух чисел в сумме дающих введенное вами число")
-except:
-    print("Вы ввели не число или дробное число")
+is_working = True
+while is_working:
+    print("Дайте мне список и число и я скажу есть ли в этом списке два числа, сумма которых дает введенное вами число")
+    try:
+        number_of_elements1 = int(input("Укажите число элементов списка: "))
+        a_list1 = [int(input("Следующий элемент списка: ")) for i in range(number_of_elements1)]
+        number_for_search1 = int(input("Укажите число которое будем проверять: "))
+        if if_sum_in_list(a_list1, number_for_search1):
+            print("В вашем списке есть два числа в сумме дающих введенное вами число")
+        else:
+            print("В вашем списке нету двух чисел в сумме дающих введенное вами число")
+        print('Перепроверим с новыми данными?; "y-да", "n-нет"')
+        if input() == 'n':
+            is_working = False
+    except:
+        print("Вы ввели не число или дробное число")
