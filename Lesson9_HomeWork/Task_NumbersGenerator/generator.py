e8d65b780365f567053aaa2f6d6a5a28ecab474d
YokaEnenra <lartaman1337@gmail.com>
2021-08-18 00:03:51 +0300

    Домашнее Задание 8, часть 3 Исправлено

--- Lesson9_HomeWork/Task_NumbersGenerator/generator.py
+++ Lesson9_HomeWork/Task_NumbersGenerator/generator.py
@@ -1,33 +1,20 @@
-def random_generate(gen_from: int = 1, gen_to: int = 100, gen_amount: int = 1):
-    generated_number = gen_to - gen_from
-    generated_numbers = list()
-    for j in range(1, gen_amount+1):
-        generated_number /= (gen_to / gen_from) / j
-        generated_number *= (gen_to * gen_from) * j
-        n = j
-        while True:
-            generated_number = round(generated_number)
-            if generated_number < gen_from:
-                # generated_number /= (gen_to / gen_from) * n * 2
-                generated_number *= (gen_to * gen_from) / n / 2
-                n += 1
-            else:
-                yield generated_number
-                generated_numbers.append(generated_number)
+def random_generate(gen_from: int = 1, gen_to: int = 100):
+    for i in range(gen_from, gen_to):
+        for j in range(2, round(i ** 0.5) + 1):
+            if i % j == 0:
                 break
-
+        else:
+            yield i
 
 print("Дайте мне 2 числа и я сгенерирую случайное число в этом диапазоне")
 try:
-    first_number = int(input("Первое число: "))
-    second_number = int(input("Второе число: "))
-    number_of_generated = int(input("Сколько чисел генерировать?: "))
-    if second_number - first_number > 0 and second_number - first_number + 1 >= number_of_generated > 0:
-        print("Я сгенерировал вот такие числа:", end=' ')
-        for i in random_generate(first_number, second_number, number_of_generated):
+    first_number = int(input("Левая граница: "))
+    second_number = int(input("Правая граница: "))
+    if second_number - first_number > 0:
+        print("В заданном диапазоне я нашел вот такие простые числа: ", end=' ')
+        for i in random_generate(first_number, second_number):
             print(i, end=' ')
     else:
-        print("Проверьте ввод, возможны ошибки: \n1)Кол-во генерируемых чисел больше промежутка чисел\n"
-              "2)Левая граница больше либо равна правой\n3)Кол-во генериремых чисел меньше или равно 0")
+        print("Проверьте ввод, левая граница больше либо равна правой")
 except:
     print("Вы ввели не число или дробное число")
