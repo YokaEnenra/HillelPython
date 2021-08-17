d7bab4bc618cf80790fc192d4c1f6dd0cda3fb45
kitayka <kitayka96@gmail.com>
2021-08-17 17:42:03 +0300

    Домашнее Задание 8, часть 2 Исправлено

--- Lesson9_HomeWork/Task_ToDegree/to_degree.py
+++ Lesson9_HomeWork/Task_ToDegree/to_degree.py
@@ -1,10 +1,10 @@
-result = lambda x, y=int(1): x ** y
-
 print("Дайте мне два числа и я возведу первое в степень второго")
+print("Сколько чисел будем возводить?")
 try:
-    first_number = int(input("Первое число: "))
-    second_number = int(input("Второе число: "))
-
-    print(result(first_number, second_number))
+    number_of_elements = int(input("Кол-во: "))
+    first_numbers = [int(input("Первое число: ")) for i in range(number_of_elements)]
+    second_numbers = [int(input("Второе число: ")) for i in range(number_of_elements)]
+    result_for_print = list(map(lambda x, y=int(1): x ** y, first_numbers, second_numbers))
+    print(result_for_print)
 except:
     print("Вы ввели не число или дробное число")
