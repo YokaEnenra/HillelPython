c396951d24eec78dbfefcae3b27ca439affb3da9
kitayka <kitayka96@gmail.com>
2021-08-17 18:08:21 +0300

    Revert "Домашнее Задание 8, часть 3"
    This reverts commit cb41f01b00061a09c0c46995cc6847b4ea2f02c8.

--- Lesson9_HomeWork/Task_NumbersGenerator/generator.py
+++ Lesson9_HomeWork/Task_NumbersGenerator/generator.py
@@ -1,32 +1,19 @@
-def generate(gen_from: int = 1, gen_to: int = 100, gen_amount: int = 1):
+def generate(gen_from: int = 1, gen_to: int = 100):
     generated_number = gen_to - gen_from
-    generated_numbers = list()
     if gen_to == gen_from or gen_to - gen_from == 1:
         return gen_to
-    tmp = generated_number
-    for i in range(gen_amount):
-        tmp /= (gen_to / gen_from) / 2
-        tmp *= (gen_to * gen_from) * 2
-        while True:
-            tmp = round(tmp)
-            if tmp > gen_to:
-                tmp /= (gen_to/gen_from)/2
-            elif tmp < gen_from:
-                tmp *= (gen_to*gen_from)*2
-            elif tmp in range(gen_from, gen_to + 1) and tmp not in generated_numbers:
-                generated_numbers.append(tmp)
-                break
-    return generated_numbers
+    while True:
+        generated_number *= 3
+        generated_number /= 5
+        generated_number = round(generated_number)
+        if generated_number in range(gen_from, gen_to + 1):
+            return generated_number
 
 
 print("Дайте мне 2 числа и я сгенерирую случайное число в этом диапазоне")
 try:
     first_number = int(input("Первое число: "))
     second_number = int(input("Второе число: "))
-    number_of_generated = int(input("Сколько чисел генерировать?: "))
-    generation_result = generate(first_number, second_number, number_of_generated)
-    print("Я сгенерировал числа:", end=' ')
-    for i in generation_result:
-        print(i, end=' ')
+    print("Я сгенерировал число:", generate(first_number, second_number))
 except:
     print("Вы ввели не число или дробное число")
