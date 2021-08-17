c8b323bfd5763f0d577a9d44f90f4317afc2a5f9
kitayka <kitayka96@gmail.com>
2021-08-17 17:51:26 +0300

    Домашнее Задание 7, часть 1 Небольшое исправление под PEP8

--- Lesson8_HomeWork/Task_DifferentSymbolsCounter/different.py
+++ Lesson8_HomeWork/Task_DifferentSymbolsCounter/different.py
@@ -3,4 +3,4 @@ import random
 elem_number = int(input("Кол-во элементов списка: "))
 my_list = [random.randint(-10, 10) for i in range(elem_number)]
 my_set = set(my_list)
-print("В полученном списке",my_list,"всего",len(my_set),"различных чисел, вот они:\n",my_set)
+print("В полученном списке", my_list, "всего", len(my_set), "различных чисел, вот они:\n", my_set)
