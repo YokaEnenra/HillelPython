f2cbfe916d133e8257600e7bb6492b9e2a039731
YokaEnenra <lartaman1337@gmail.com>
2021-08-18 00:05:30 +0300

    Домашнее Задание 8, часть 3 Исправление на соответствие PEP8

--- Lesson9_HomeWork/Task_NumbersGenerator/generator.py
+++ Lesson9_HomeWork/Task_NumbersGenerator/generator.py
@@ -1,10 +1,11 @@
 def random_generate(gen_from: int = 1, gen_to: int = 100):
-    for i in range(gen_from, gen_to):
-        for j in range(2, round(i ** 0.5) + 1):
-            if i % j == 0:
+    for k in range(gen_from, gen_to):
+        for n in range(2, round(k ** 0.5) + 1):
+            if k % n == 0:
                 break
         else:
-            yield i
+            yield k
+
 
 print("Дайте мне 2 числа и я сгенерирую случайное число в этом диапазоне")
 try:
@@ -16,5 +17,5 @@ try:
             print(i, end=' ')
     else:
         print("Проверьте ввод, левая граница больше либо равна правой")
-except:
+except ValueError:
     print("Вы ввели не число или дробное число")
