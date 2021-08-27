5f5d53a7c73ee156db4077d6018f066511e415b2
YokaEnenra <lartaman1337@gmail.com>
2021-08-27 12:39:48 +0300

    Домашнее задание 7, часть 2 Исправлено

--- Lesson8_HomeWork/Task_DifferentSymbolsCounterTwoLists/different_two.py
+++ Lesson8_HomeWork/Task_DifferentSymbolsCounterTwoLists/different_two.py
@@ -4,5 +4,5 @@ elem_number1 = int(input("Кол-во элементов первого спис
 elem_number2 = int(input("Кол-во элементов второго списка: "))
 my_list1 = [random.randint(-10, 10) for i in range(elem_number1)]
 my_list2 = [random.randint(-10, 10) for j in range(elem_number2)]
-my_set = set(my_list1 + my_list2)
-print("В полученных списках", my_list1, my_list2, "всего", len(my_set), "различных чисел, вот они:\n", my_set)
+my_set = set(i for i in my_list1 if i in my_list2)
+print("В первом полученном списке", my_list1, "всего", len(my_set), "различных чисел, встречающихся во втором списке", my_list2,"вот они:\n", my_set)
