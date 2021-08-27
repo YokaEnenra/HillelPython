fbf34034f3f72817912cd55287c6a64c8cf8663b
YokaEnenra <lartaman1337@gmail.com>
2021-08-27 14:18:16 +0300

    Домашнее задание 10, часть 1 Выполнено

---
+++ Lesson11_HomeWork/Task_ReverseNumber/README.md
@@ -0,0 +1,4 @@
+There is first part of my tenth homework.
+My task was to create program that will ask user a number and then reverse it using only simple math operations
+, using self-writen functions and recursion.
+
---
+++ Lesson11_HomeWork/Task_ReverseNumber/reverse_number.py
@@ -0,0 +1,17 @@
+import math
+
+
+def reverse_it(n: int) -> int:
+    if n > 10:
+        current_number = n % 10
+        not_reversed_numbers = n // 10
+        degree_counter = int(math.log10(not_reversed_numbers) + 1)
+
+        return current_number * 10 ** degree_counter + reverse_it(not_reversed_numbers)
+    else:
+        return n
+
+
+counter = int(input("Сколько чисел будем переворачивать? "))
+for i in range(counter):
+    print(reverse_it(int(input("Введите число: "))))
