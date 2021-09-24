28fe3f1c03f1fd8d157349c05d63b7ba18a748d3
YokaEnenra <lartaman1337@gmail.com>
2021-09-24 12:29:20 +0300

    Домашнее задание 10 Выполнено

--- Lesson11_HomeWork/Task_TreangleSequence/treangle_sequence.py
+++ Lesson11_HomeWork/Task_TreangleSequence/treangle_sequence.py
@@ -1,12 +1,12 @@
 def count_first(nums, n: int):
-    sum_to_return = 0
+    summ = 0
     j = 0
     if n == 1:
         print('1', end='')
     else:
         i = 1
-        while sum_to_return < n:
-            sum_to_return += i
+        while summ < n:
+            summ += i
             j = i
             i += 1
         print(count_first(None, n - 1), j, end='')
